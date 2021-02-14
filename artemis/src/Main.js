1730dd5edfb09f0e2539f0bb037e370a2e174a29
colejagdtiger <coleton.kotch@gmail.com>
2021-02-13 20:06:51 -0700

    added apiGoogleconfig.json

--- artemis/src/Main.js
+++ artemis/src/Main.js
@@ -1,5 +1,6 @@
-import react from 'react';
+import react from 'react'
 import MainNavbar from './MainNavbar.js'
+import ApiCalendar from 'react-google-calendar-api'
 
 export default function Main(props) {
 
@@ -9,6 +10,25 @@ export default function Main(props) {
             <MainNavbar setTab={props.setTab}/>
             {/* <button onClick={()=>clickEvent()}></button> */}
             {props.children}
+	          <button
+              onClick={(e) => handleItemClick(e, 'sign-in')}
+            >
+              sign-in
+            </button>
+            <button
+                onClick={(e) => handleItemClick(e, 'sign-out')}
+            >
+              sign-out
+            </button>
         </div>
+
     )
+}
+
+const handleItemClick = (event, name) => {
+      if (name === 'sign-in') {
+	      ApiCalendar.handleAuthClick()
+      } else if (name === 'sign-out') {
+        ApiCalendar.handleSignoutClick()
+      }
 }
