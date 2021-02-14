a05bc92de76e93c66d7563b9cb9a8675e4eb7a1d
Zeyu-Li <zeyu.li23@gmail.com>
2021-02-13 18:51:47 -0600

    finished tabs

--- artemis/src/App.js
+++ artemis/src/App.js
@@ -1,3 +1,4 @@
+import react, {Component, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Main from './Main.js'
@@ -5,13 +6,25 @@ import Calendar from './Calendar.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const [tab, setTab] = useState("home")
+
+  const doTab = () => {
+    if (tab == "home") {
+      return <Calendar />
+    } else if (tab == "summary") {
+      return <p>Summary</p>
+    } else {
+      return <p>Setting</p>
+    }
+  }
+
   return (
     <div className="App">
-      <Main>
-        <Calendar />
+      <Main setTab={setTab}>
+        {doTab()}
       </Main>
     </div>
-  );
+  )
 }
 
 export default App;
--- artemis/src/Main.js
+++ artemis/src/Main.js
@@ -2,19 +2,12 @@ import react from 'react';
 import MainNavbar from './MainNavbar.js'
 
 export default function Main(props) {
-    const style = {
-        marginTop: "5vh",
-        marginLeft: "50px",
-        marginRight: "50px",
-        textAlign: "left",
-    }
+
 
     return (
         <div>
-            <MainNavbar />
-            <div style={style}>
-                <h1 style={{fontSize: "4em"}}>Hello User</h1>
-            </div>
+            <MainNavbar setTab={props.setTab}/>
+            {/* <button onClick={()=>clickEvent()}></button> */}
             {props.children}
         </div>
     )
